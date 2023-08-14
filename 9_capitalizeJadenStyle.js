3d0a8280383fb04df021e895bd7279e4c19ae328
Dmytro <2dmitri.akimov@gmail.com>
2023-08-14 04:00:31 +0200

    capitalizeJadenStyle

--- 9_capitalizeJadenStyle.js
+++ 9_capitalizeJadenStyle.js
@@ -1,18 +1,17 @@
 function capitalizeJadenStyle(el) {
-    return el.split(' ')
-        .map((word) => {
-            if (word.length > 0) {
-                const firstLetter = word[0].toUpperCase();
-                return firstLetter + word.slice(1);
-            } else {
-                return word;
-            }
-        })
-        .join(' ');
+  return el
+    .split(" ")
+    .map((word) => {
+      if (word.length > 0) {
+        const firstLetter = word[0].toUpperCase();
+        return firstLetter + word.slice(1);
+      } else {
+        return word;
+      }
+    })
+    .join(" ");
 }
 
 const originalQuote = "how can mirrors be real if our eyes aren't real";
 const jadenStyleQuote = capitalizeJadenStyle(originalQuote);
 console.log(jadenStyleQuote);
-
-

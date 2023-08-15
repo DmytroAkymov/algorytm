889138ca57c58f16948d5a53e80acaf87b4234de
Dmytro <2dmitri.akiomv@gmail.com>
2023-08-15 17:14:15 +0200

    9_update

--- 9_capitalizeJadenStyle.js
+++ 9_capitalizeJadenStyle.js
@@ -1,5 +1,4 @@
 function capitalizeJadenStyle(el) {
-<<<<<<< HEAD
     return el
         .split(' ')
         .map((word) => {
@@ -11,19 +10,6 @@ function capitalizeJadenStyle(el) {
             }
         })
         .join(' ');
-=======
-  return el
-    .split(" ")
-    .map((word) => {
-      if (word.length > 0) {
-        const firstLetter = word[0].toUpperCase();
-        return firstLetter + word.slice(1);
-      } else {
-        return word;
-      }
-    })
-    .join(" ");
->>>>>>> 3d0a8280383fb04df021e895bd7279e4c19ae328
 }
 
 const originalQuote = "how can mirrors be real if our eyes aren't real";
